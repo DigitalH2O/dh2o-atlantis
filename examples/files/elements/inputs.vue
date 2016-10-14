96195b67a026fced8d9df1c67d9af8d43515f962
Luke Savage <lsavage@digitalh2o.com>
2016-10-14 13:35:19 -0500

    added checkbox, label styles and updated some spacing"

--- examples/files/elements/inputs.vue
+++ examples/files/elements/inputs.vue
@@ -6,28 +6,26 @@
         In order to keep things simple default styles have been added
         to input fields and there types
       </p>
-      <label>
-        Text
-        <input type="text" placeholder="Text" />
-      </label>
-      <label>
-        Email
-        <input type="email" placeholder="Email" />
-      </label>
-      <label>
-        Password
-        <input type="password" placeholder="Password" />
-      </label>
-      <label>
-        Search
-        <input type="text" class="search" placeholder="Search" />
-      </label>
-      <label>
-        Textarea
-        <textarea name="name" rows="3" placeholder="Textarea"></textarea>
-      </label>
+      <label for="text-input">Text</label>
+      <input type="text" id="text-input" placeholder="Text" />
+      <label for="email-input">Email</label>
+      <input type="email" id="email-input" placeholder="Email" />
+      <label for="password-input">Password</label>
+      <input type="password" id="password-input" placeholder="Password" />
+      <label for="search-input">Search</label>
+      <input type="text" id="search-input" class="search" placeholder="Search" />
+      <label for="text-area-input">Textarea</label>
+      <textarea name="name" id="text-area-input" rows="3" placeholder="Textarea"></textarea>
+      <label>Checkbox</label>
+      <fieldset>
+        <input id="checkbox-input1" type="checkbox"/>
+        <label for="checkbox-input1" class="inline checkbox-label">Option 1</label>
+        <input id="checkbox-input2" type="checkbox"/>
+        <label for="checkbox-input2" class="inline checkbox-label">Option 2</label>
+        <input id="checkbox-input3" type="checkbox"/>
+        <label for="checkbox-input3" class="inline checkbox-label">Option 3</label>
+      </fieldset>
       <div class="message notice">Switching input types sets icons</div>
     </div>
   </div>
-
 </template>
