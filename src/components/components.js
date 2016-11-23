19f0879b5c627e19e605e5591b1396e43c66e967
Luke Savage <lsavage@digitalh2o.com>
2016-11-23 14:32:02 -0600

    converted let to var as a test

--- src/components/components.js
+++ src/components/components.js
@@ -4,7 +4,7 @@ var dh2oDropdown = require('./dropdowns/dropdowns')
 // This will be reusable for our modals/dropdowns and anything else that needs to
 // be shown in front of other stacking contexts
 window.findParentStackingContext = function (initialEle) {
-  let lastParentEle
+  var lastParentEle
 
   function innerClosure (ele) {
     if (ele.classList.contains('app')) {
@@ -30,7 +30,7 @@ window.findParentStackingContext = function (initialEle) {
 // This allows you to look up a childs parents recursively to determine if
 // the child element has a parent with the specified className, if so it returns the parent ele
 window.getParentElement = function (initialEle, parentClass) {
-  let parentEle
+  var parentEle
   function findParent (ele) {
     if (!ele) {
       return
