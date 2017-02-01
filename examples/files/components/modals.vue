03db1af18c3845ae6d73f708a1b323fc9f5b9912
brianvoe <brian@webiswhatido.com>
2017-02-01 14:23:03 -0600

    modals - minor animation adjustment and example fix

--- examples/files/components/modals.vue
+++ examples/files/components/modals.vue
@@ -1,11 +1,11 @@
 <script>
-  import { dh2oModal } from 'dh2o-atlantis/components/components.js'
+  import dh2oModal from 'dh2o-atlantis/components/modals/modals.js'
   export default {
     methods: {
-      applyModal: function () {
+      applyModal: () => {
         var text = '<h1>Great job on clicking a button. What?!?! Do you want a medal or something?</h1>'
         document.querySelector('#modal2 .modal-body').innerHTML = text
-        setTimeout(function () {
+        setTimeout(() => {
           dh2oModal('modal2').hide()
         }, 1000)
       }
