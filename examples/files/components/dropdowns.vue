24539343c6ca381800065207faba711034811427
brianvoe <brian@webiswhatido.com>
2017-02-02 12:04:06 -0600

    dropdowns - updated to be able to programmably trigger dropdowns and some other minor cleanup.

--- examples/files/components/dropdowns.vue
+++ examples/files/components/dropdowns.vue
@@ -1,4 +1,24 @@
-<style>
+<script>
+  import Vue from 'vue'
+  import dh2oDropdown from 'dh2o-atlantis/components/dropdowns/dropdowns.js'
+  export default {
+    data () {
+      return {
+        showDropdown: false
+      }
+    },
+    methods: {
+      click () {
+        this.showDropdown = true
+        Vue.nextTick(function () {
+          dh2oDropdown('program_trigger')
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
   #content-dropdowns {
     .code-sample {
       .as-rendered, .as-code {
@@ -6,9 +26,14 @@
         width: 100%;
       }
     }
+
+    #program_trigger {
+      .dh2o-dropdown-content {
+        padding: 10px;
+      }
+    }
   }
 </style>
-
 
 <template>
   <div id="content-dropdowns" class="section">
@@ -21,8 +46,8 @@
         For the items in the dropdown menu, apply the "menu-item" class to get the hover states and borders as shown below.
       </p>
       <code-sample>
-        <div class="dh2o-dropdown-trigger btn">
-          <div class="dh2o-dropdown-trigger-text">User Menu</div>
+        <div class="dh2o-dropdown-container">
+          <div class="dh2o-dropdown-trigger btn"><span>User Menu</span></div>
           <div class="dh2o-dropdown-content">
             <ul>
               <li class="menu-item">User Account</li>
@@ -30,7 +55,17 @@
             </ul>
           </div>
         </div>
-        </code-sample>
+      </code-sample>
+
+      <p>You can also programmably trigger showing the dropdown</p>
+      <code-sample>
+        <div id="program_trigger" class="dh2o-dropdown-container">
+          <div class="btn" @click="click">Button</div>
+          <div v-if="showDropdown" class="dh2o-dropdown-content">
+            Stuff to show here
+          </div>
+        </div>
+      </code-sample>
 
       <div class="message alert">
         Do not use button element for dropdowns.
