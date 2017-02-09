e62722f9ae7e3476395fdfecbb647d18d2011583
brianvoe <brian@webiswhatido.com>
2017-02-09 10:51:16 -0600

    modals - added esc key implementation

--- src/components/modals/modals.js
+++ src/components/modals/modals.js
@@ -66,4 +66,23 @@ document.addEventListener('click', function (event) {
   }
 })
 
+// Close last modal when pressing esc
+document.onkeydown = function (evt) {
+  evt = evt || window.event
+  var isEscape = false
+  if ('key' in evt) {
+    isEscape = (evt.key === 'Escape' || evt.key === 'Esc')
+  } else {
+    isEscape = (evt.keyCode === 27)
+  }
+  if (isEscape) {
+    var modals = document.querySelectorAll('.dh2o-modal.animate-in')
+    if (modals.length > 0) {
+      if (modals[modals.length - 1].hide) {
+        modals[modals.length - 1].hide()
+      }
+    }
+  }
+}
+
 export default dh2oModal
