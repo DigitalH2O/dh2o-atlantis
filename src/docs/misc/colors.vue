b96bff3df99df68771ade36a52d031257740940e
David Jackson <altmail@jackson-wallace.net>
2017-04-27 11:17:16 -0500

    Move colors into a helper js file exposed in the distribution

--- src/docs/misc/colors.vue
+++ src/docs/misc/colors.vue
@@ -1,54 +1,17 @@
 <script>
-  import tinycolor from 'tinycolor2'
-  import colorVars from '!!sass-extract-loader!../../../src/scss/misc/_colors.scss'
+  import colors from 'src/helpers/colors'
 
   export default {
     data () {
-      const darkerPercent = colorVars.global['$darker-percent'].value
-      const darkestPercent = colorVars.global['$darkest-percent'].value
-      const baseColors = []
-      const messageColors = []
-      const textColors = []
-      const assetColors = []
-      const darkerColors = []
-      const darkestColors = []
-      let colors = colorVars.global.$colors.value
-      let colorsBase = colors.base.value
-      let colorsMessage = colorVars.global['$message-colors'].value
-      let colorsText = colorVars.global['$text-colors'].value
-      let colorsAsset = colorVars.global['$asset-colors'].value
-
-      for (let name in colorsBase) {
-        baseColors.push({name: name, value: colorsBase[name].value.hex})
-        darkerColors.push({name: name, value: tinycolor(colorsBase[name].value.hex).darken(darkerPercent).toString()})
-        darkestColors.push({name: name, value: tinycolor(colorsBase[name].value.hex).darken(darkerPercent).toString()})
-      }
-
-      for (let name in colorsMessage) {
-        messageColors.push({name: name, value: colorsMessage[name].value.hex})
-      }
-
-      for (let name in colorsText) {
-        textColors.push({name: name, value: colorsText[name].value.hex})
-      }
-
-      for (let name in colorsAsset) {
-        assetColors.push({
-          name: name,
-          hex: colorsAsset[name].value.color.value.hex,
-          icon: colorsAsset[name].value.icon.value
-        })
-      }
-
       return {
-        baseColors,
-        messageColors,
-        textColors,
-        assetColors,
-        darkerPercent,
-        darkestPercent,
-        darkerColors,
-        darkestColors
+        baseColors: colors.baseColors,
+        messageColors: colors.messageColors,
+        textColors: colors.textColors,
+        assetColors: colors.assetColors,
+        darkerPercent: colors.darkerPercent,
+        darkestPercent: colors.darkestPercent,
+        darkerColors: colors.darkerColors,
+        darkestColors: colors.darkestColors
       }
     },
     methods: {
@@ -133,22 +96,22 @@
 
           <h4>Base</h4>
           <div class="colors">
-            <div class="item" v-for="color in baseColors">
-              <div class="color" :style="{'background-color': color.value}"></div>
+            <div class="item" v-for="color, name in baseColors">
+              <div class="color" :style="{'background-color': color}"></div>
               <div class="text">
-                <div class="name">${{color.name}}-color</div>
-                <div class="hex">{{color.value}}</div>
+                <div class="name">${{name}}-color</div>
+                <div class="hex">{{color}}</div>
               </div>
             </div>
           </div>
 
           <h4>Text</h4>
           <div class="colors">
-            <div class="item" v-for="color in textColors">
-              <div class="color" :style="{'background-color': color.value}"></div>
+            <div class="item" v-for="color, name in textColors">
+              <div class="color" :style="{'background-color': color}"></div>
               <div class="text">
-                <div class="name">${{color.name}}-color</div>
-                <div class="hex">{{color.value}}</div>
+                <div class="name">${{name}}-color</div>
+                <div class="hex">{{color}}</div>
               </div>
             </div>
           </div>
@@ -162,11 +125,11 @@
           <p>The message color palette is used in the slide-out messaging bar.</p>
 
           <div class="colors">
-            <div class="item" v-for="color in messageColors">
-              <div class="color" :style="{'background-color': color.value}"></div>
+            <div class="item" v-for="color, name in messageColors">
+              <div class="color" :style="{'background-color': color}"></div>
               <div class="text">
-                <div class="name">${{color.name}}-color</div>
-                <div class="hex">{{color.value}}</div>
+                <div class="name">${{name}}-color</div>
+                <div class="hex">{{color}}</div>
               </div>
             </div>
           </div>
@@ -180,12 +143,12 @@
           <p>Assets are assigned a color (and icon) which is used to highlight and visually communicate that assets parent definition.</p>
 
           <div class="colors">
-            <div class="item" v-for="color in assetColors">
+            <div class="item" v-for="color, name in assetColors">
               <div class="color" :style="{'background-color': color.hex}"></div>
               <div class="text">
                 <div class="icon" v-html="icon(color)"></div>
                 <div class="fa">{{color.icon}}</div>
-                <div class="name">${{color.name}}-color</div>
+                <div class="name">${{name}}-color</div>
                 <div class="hex">{{color.hex}}</div>
               </div>
             </div>
