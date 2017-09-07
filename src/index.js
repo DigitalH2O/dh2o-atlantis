d804e8f6c9cfc6d310a552a895c3c9c6a380b45a
brianvoe <brian@webiswhatido.com>
2017-09-07 15:58:53 -0500

    src - import update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import dh2oDashboardsClass from './components/dashboards/index.js'
-import dh2oModalFunction from './components/modals/modals.js'
+import dh2oModalClass from './components/modals/modals.ts'
 import dh2oDropdownFunction from './components/dropdowns/dropdowns.js'
 import dh2oLoadingComponent from './components/loading/loading.vue'
 import dh2oNoDataComponent from './components/nodata/nodata.vue'
 import allHelpers from './helpers'
 
 export const dh2oDashboards = dh2oDashboardsClass
-export const dh2oModal = dh2oModalFunction
+export const dh2oModal = dh2oModalClass
 export const dh2oDropdown = dh2oDropdownFunction
 export const dh2oLoading = dh2oLoadingComponent
 export const dh2oNoData = dh2oNoDataComponent
