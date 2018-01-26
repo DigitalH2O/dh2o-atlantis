d58adffadb326eb9f99c3ee4ceeb850dd83cd2b8
brianvoe <brian@webiswhatido.com>
2018-01-26 09:34:47 -0600

    output - dist docs update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dh2oDashboardsClass from './components/dashboards/index.js'
 import dh2oModalClass from './components/modals/modals.ts'
 import dh2oDropdownFunction from './components/dropdowns/dropdowns.js'
+import dh2oDrawerClass from './components/drawer/drawer.ts'
 import dh2oLoadingComponent from './components/loading/loading.vue'
 import dh2oNoDataComponent from './components/nodata/nodata.vue'
 import allHelpers from './helpers'
@@ -8,6 +9,7 @@ import allHelpers from './helpers'
 export const dh2oDashboards = dh2oDashboardsClass
 export const dh2oModal = dh2oModalClass
 export const dh2oDropdown = dh2oDropdownFunction
+export const dh2oDrawer = dh2oDrawerClass
 export const dh2oLoading = dh2oLoadingComponent
 export const dh2oNoData = dh2oNoDataComponent
 export const helpers = allHelpers
