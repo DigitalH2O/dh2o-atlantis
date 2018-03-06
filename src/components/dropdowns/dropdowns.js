262ac1e561b1e0fd559fa40d76f14529a7e0af80
brianvoe <brian@webiswhatido.com>
2018-03-06 12:41:51 -0600

    lint - removed callback literal

--- src/components/dropdowns/dropdowns.js
+++ src/components/dropdowns/dropdowns.js
@@ -21,7 +21,7 @@ function hasClassInTree (element, className) {
   return hasClass(element, className) || parentByClass(element, className)
 }
 
-var dh2oDropdown = function (dropdown, autoShow = true, callbackFunc = null) {
+var dh2oDropdown = function (dropdown, autoShow = true, callback = null) {
   var contentStr = 'dh2o-dropdown-content'
 
   // If you are passing in a string get element from it
@@ -40,7 +40,7 @@ var dh2oDropdown = function (dropdown, autoShow = true, callbackFunc = null) {
   var content = dropdown.getElementsByClassName(contentStr)[0]
   dropdown.show = function () {
     if (content) {
-      if (callbackFunc) { callbackFunc(true) }
+      if (callback) { callback(true) }
       content.classList.remove('animate-out')
       content.classList.add('animate-in')
     }
@@ -50,7 +50,7 @@ var dh2oDropdown = function (dropdown, autoShow = true, callbackFunc = null) {
       content.classList.remove('animate-in')
       content.classList.add('animate-out')
       setTimeout(function () {
-        if (callbackFunc) { callbackFunc(false) }
+        if (callback) { callback(false) }
         content.classList.remove('animate-out')
       }, 300)
     }
